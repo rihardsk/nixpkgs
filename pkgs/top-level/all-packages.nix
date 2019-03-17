f81ac3e605cbadbb228884b35acb09ff1d052c02
Tom Hall <tahall256@protonmail.ch>
2019-03-17 14:26:43 +0000

    init nix-user-chroot

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23767,4 +23767,29 @@ in
       runHook postInstall
     '';
   };
+
+  nix-user-chroot = stdenv.lib.makeOverridable stdenv.mkDerivation {
+    name = "nix-user-chroot-2c52b5f";
+    src = ~/nix-bundle/nix-user-chroot;
+
+    makeFlags = [];
+
+    # hack to use when /nix/store is not available
+#    postFixup = ''
+#      exe=$out/bin/nix-user-chroot
+#      patchelf \
+#        --set-interpreter .$(patchelf --print-interpreter $exe) \
+#        --set-rpath $(patchelf --print-rpath $exe | sed 's|/nix/store/|./nix/store/|g') \
+#        $exe
+#    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin/
+      cp nix-user-chroot $out/bin/nix-user-chroot
+
+      runHook postInstall
+    '';
+  };
 }
